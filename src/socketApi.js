a90acd65da24b582e5ee2c51ee2f46df91d5b63d
unknown <gecin18@gmail.com>
2019-03-24 00:31:07 +0300

    online kullanicilarin id leri ile birlikde redisle baglantisi

--- src/socketApi.js
+++ src/socketApi.js
@@ -6,11 +6,15 @@ const socketApi = {
     io
 };
 
+//libs (online kullanicinin redisdeki verisi)
+const Users = require('./lib/Users');
+
 //socket authorization (redisdeki session verisini socket io ya baglayabilmeye yariyor)
 io.use(socketAuthorization);
 
 io.on('connection', socket =>{
    console.log('Hos geldin '+ socket.request.user.name);
+    Users.upsert(socket.id, socket.request.user);
 
     socket.on('disconnect', socket =>{
         console.log('bir kullanici ayrildi');
