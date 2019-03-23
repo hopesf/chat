3c805716ff191c6ecd8b7a20812d42e1b6bfd01b
unknown <gecin18@gmail.com>
2019-03-23 06:57:12 +0300

    redis ve session socket authorizationu yapildi

--- src/socketApi.js
+++ src/socketApi.js
@@ -1,12 +1,21 @@
 const socketio = require("socket.io");
+const socketAuthorization = require('../middleware/socketAuthorization');
 const io = socketio();
 
 const socketApi = {
     io
 };
 
+//socket authorization (redisdeki session verisini socket io ya baglayabilmeye yariyor)
+io.use(socketAuthorization);
+
 io.on('connection', socket =>{
-   console.log('bir kullanici baglandi');
+   console.log('Hos geldin '+ socket.request.user.name);
+
+    socket.on('disconnect', socket =>{
+        console.log('bir kullanici ayrildi');
+    });
 });
+
 
 module.exports = socketApi;
