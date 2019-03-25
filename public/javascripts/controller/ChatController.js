a18f196c560f82d9841b065f3d4deb936cae3b93
unknown <gecin18@gmail.com>
2019-03-25 05:31:26 +0300

    anlik olarak ui da kullanicilari gosterebiliyoruz.

--- public/javascripts/controller/ChatController.js
+++ public/javascripts/controller/ChatController.js
@@ -1,11 +1,15 @@
 app.controller('ChatController', ['$scope',($scope) =>{
-    $scope.onlineList = [];
+    $scope.onLineList = [];
     $scope.activeTab = 2;
-
     $scope.changeTab = tab =>{
             $scope.activeTab = tab;
     };
 
-
     const socket = io.connect("http://localhost:3000");
+
+    socket.on('onlineList', users => {
+        $scope.onlineList = users;
+        $scope.$apply();
+    });
+
 }]);
